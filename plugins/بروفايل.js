962b62c2d5f24ef0aab2052339745224473615b1
kam0998 <ztrlz06@gmail.com>
2024-09-10 16:12:55 +0200

    Update بروفايل.js

--- plugins/بروفايل.js
+++ plugins/بروفايل.js
@@ -1,45 +1,30 @@
-import { createHash } from 'crypto'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-
-let handler = async (m, { conn, usedPrefix, command}) => {
-
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Menu.jpg')
-let user = global.db.data.users[who]
-let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || ''
-let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } = global.db.data.users[who]
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let username = conn.getName(who)
-let math = max - xp 
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-
-// • @${who.replace(/@.+/, '')}
-let str = `*🪪 الإسم:* ${username}${about ? '\n\n 🎌 *السيرة:* ' + about : ''}
-
-*⚠️ التحذيرات:* ${warn}/5
-
-*💰 الذهب :* ${credit}
-
-*✨ المستوى* : ${level}
-
-*⬆️ الخبرة* : إجمالي ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `جاهز لـ *${usedPrefix}تطوير*` : `*${math}xp* ناقصة للتطوير`}
-
-*🏆 الترتيب:* ${role}
-
-*📇 مسجل :* ${registered ? 'نعم': 'لا'}
-
-*⭐ بريميوم* : ${prem ? 'نعم' : 'لا'}
-`
-    await conn.sendMessage(m.chat, { react: { text: '🌚', key: m.key } })
-
-    conn.sendFile(m.chat, pp, 'profil.jpg', str, m, false, { mentions: [who] })
-
-
-}
-handler.help = ['profile']
-handler.tags = ['group']
-handler.command = ['بروفايل', 'انا', 'بروفايلي'] 
-
-export default handler
+import {createHash} from 'crypto';
+import PhoneNumber from 'awesome-phonenumber';
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
+  let pp = 'https://telegra.ph/file/cf3ff642dd9eb7b209e36.jpg';
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  if (!(who in global.db.data.users)) throw `المستخدم الذي ذكرته غير مسجل في قاعدة البيانات الخاصة بي`;
+  try {
+    pp = await conn.profilePictureUrl(who);
+  } catch (e) {
+  } finally {
+    const {name, limit, lastclaim, registered, regTime, age, premiumTime} = global.db.data.users[who];
+    const username = conn.getName(who);
+    const prem = global.prems.includes(who.split `@` [0]);
+    const sn = createHash('md5').update(who).digest('hex');
+    const str = `*اســمــك:* ${username} ${registered ? '(' + name + ') ': ''}
+*رقـــمــك:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*رابطك:* wa.me/${who.split`@`[0]}${registered ? '\n*العــمر:* ' + age + ' سنه' : ''}
+*الـمــاسـك:* ${limit} 𝚄𝚂𝙾𝚂
+*مــســجــل:* ${registered ? 'نعم': 'لا'}
+*بـريـمــيــوم:* ${premiumTime > 0 ? 'يب' : (isPrems ? 'يب' : 'لا') || ''}
+*الايدي:* 
+${sn}`;
+    conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
+  }
+};
+handler.help = ['profile [@user]'];
+handler.tags = ['xp'];
+handler.command = /^بروفايل|بروفايلي?$/i;
+export default handler;
