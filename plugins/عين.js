7cbd835fd6b57352cdde6aadef192b6c46d64c7a
kam0998 <ztrlz06@gmail.com>
2024-09-10 10:59:45 +0200

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -1,31 +1,69 @@
-let timeout = 60000
-let poin = 500
-let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-    let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '*⌬ ❛╏لم يتم الاجابة علي السؤال بعد┃❌ ❯*', conn.tebakbendera[id][0])
-        throw false
-    }
-    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/4e01c190b7d67225ad7a86d388eeedf6/raw/67f0de059cea4b965a3f3bf211c12fc9c48043e5/gistfile1.txt')).json()
-  let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}*
-  *⌬ ❛╏الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)} ┇*
-  *استخدم انسحب للأنسحاب*
-  *⌬ ❛╏الـجـائـزة💰↞ ${poin} نقاط┇*
-      *⌬ ❛╏ 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡 ╏*
-     `.trim()
-    conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img, '', caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n*❐↞┇الاجـابـة✅↞ ${json.name}┇*`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
-        }, timeout)
-    ]
-}
-handler.help = ['عين']
-handler.tags = ['fun']
-handler.command = /^عين/i
+import fs from 'fs';
+import axios from 'axios';
 
-export default handler
+let timeout = 60000;
+let poin = 500;
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+
+    let id = m.chat;
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, `
+╮───────────────────────╭ـ
+│ *في سؤال لسه مجاوبتش عليه يا فاشل* ┃❌ ❯
+╯───────────────────────╰ـ`.trim(), conn.tekateki[id][0]);
+        throw false;
+    }
+
+    try {
+        const fileId = '19G3t3NszU_1Y3Uu2ri5mQ_xTL_Ar2E-T';
+        const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
+        const res = await axios.get(url);
+
+        if (res.data && Array.isArray(res.data)) {
+            let tekateki = res.data;
+            let json = tekateki[Math.floor(Math.random() * tekateki.length)];
+            
+            let _clue = json.response;
+            let clue = _clue.replace(/[A-Za-z]/g, '_');
+            let img = json.image || 'https://telegra.ph/file/034daa6dcfb2270d7ff1c.jpg';
+            let answer = json.response;
+             let questions = json.question || 'من هو هذا ؟';
+             
+
+            let caption = `
+╮───────────────────────╭ـ
+│ ❓ *السـؤال : ${questions}*
+│ ⏳ *الـوقـت : ${(timeout / 1000).toFixed(2)}*
+│ 💰 *الـجـائـزة : ${poin} نقطه*
+│ 🏳️ *الانسـحاب : استخدم [انسحاب] للانسحاب من اللعبة*
+╯───────────────────────╰ـ`.trim();
+
+            conn.tekateki[id] = [
+                await conn.sendMessage(m.chat, { image: { url: img }, caption: caption }, { quoted: m }),
+                json, poin,
+                setTimeout(async () => {
+                    if (conn.tekateki[id]) await conn.reply(m.chat, `
+╮───────────────────────╭ـ
+│ ❎ *خلص الوقت وانت زي منت فاشل مجوبتش*
+│ ✅ *الاجابه هي : ${answer}*
+╯───────────────────────╰ـ`.trim(), conn.tekateki[id][0]);
+
+                    delete conn.tekateki[id];
+                }, timeout)
+            ];
+
+        } else {
+            console.error('The received data is not a valid JSON array.');
+        }
+    } catch (error) {
+        console.error('Error fetching data from Google Drive:', error);
+    }
+};
+
+handler.help = ['acertijo'];
+handler.tags = ['game'];
+handler.command = /^(عين)$/i;
+
+export default handler;
