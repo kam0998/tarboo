39af593f7052cd8bb14cddb4cca5f53da2e09bdc
kam0998 <ztrlz06@gmail.com>
2024-09-10 10:22:41 +0200

    Update _antiprivate.js

--- plugins/_antiprivate.js
+++ plugins/_antiprivate.js
@@ -12,7 +12,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply('ممنوع مراسلة البوت خاص حتي لايتم تبنيد الرقم سيتم حظرك الان\n\nجروب الدعم لو حابب تستخدم البوت\nhttps://chat.whatsapp.com/CcQr4yKaYOL6R6608bDdKp', false, {mentions: [m.sender]});
+    await m.reply('ممنوع مراسلة البوت خاص حتي لايتم تبنيد الرقم سيتم حظرك الان\n\nجروب الدعم لو عايز تستخدم البوت\nhttps://chat.whatsapp.com/HEymhaMK6EfD4MRWj00juf', false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
