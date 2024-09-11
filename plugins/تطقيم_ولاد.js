4d7ad62df84fe67795f6e765c5091ae698a11c04
kam0998 <ztrlz06@gmail.com>
2024-09-11 03:58:19 +0200

    Update تطقيم_ولاد.js

--- plugins/تطقيم_ولاد.js
+++ plugins/تطقيم_ولاد.js
@@ -1,23 +1,17 @@
-import fs from 'fs/promises';
-
+import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
-  try {
-    // قراءة ملف JSON من المسار المحلي
-    let data = JSON.parse(await fs.readFile('src/game/طقم2.json', 'utf-8'));
-    let cita = data[Math.floor(Math.random() * data.length)];
 
-    let cowi = await (await fetch(cita.cowo)).buffer();
-    await conn.sendFile(m.chat, cowi, '', '*❮ ولـد ❯ ✓*\n> 🅃🄰🅁🄱🄾🄾☞🄱🄾🅃', m);
-    let ciwi = await (await fetch(cita.cewe)).buffer();
-    await conn.sendFile(m.chat, ciwi, '', '*❮ ولـد ❯ ✓*\n> 🅃🄰🅁🄱🄾🄾☞🄱🄾🅃', m);
-  } catch (error) {
-    console.error(error);
-  }
+  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/4fe0cb9c04d00d9b630b0e7a47b82cee46e90db0/xjsjjdjdjsd.json')).json()
+  let cita = data[Math.floor(Math.random() * data.length)]
+  
+  let cowi = await(await fetch(cita.sjdbs)).buffer()
+  await conn.sendFile(m.chat, cowi, '⛩️┃🏮MIDO🏮┃⛩️', '👤', m)
+  let ciwi = await(await fetch(cita.hsvqwiw)).buffer()
+  await conn.sendFile(m.chat, ciwi, '⛩️┃🏮MIDO🏮┃⛩️', '👤', m)
 }
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['internet']
+handler.command = ['تطقيم-اولاد','طقم-اولاد'] 
 
-handler.help = ['Miku bot'];
-handler.tags = ['Miku bot'];
-handler.command = /^طقم2|طقم-اولاد|تطقيم_ولاد$/i;
-handler.limit = true;
 
-export default handler;
+export default handler
