6f2e749f5b7bde1e2d5ae3c45034092917326239
kam0998 <ztrlz06@gmail.com>
2024-09-12 01:16:56 +0200

    Update علم2.js

--- plugins/علم2.js
+++ plugins/علم2.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-  let id = m.chat
-  if (!(id in conn.tebakbendera)) throw false
-  let json = conn.tebakbendera[id][1]
-  conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (!(id in conn.tebakbendera)) throw false
+    let json = conn.tebakbendera[id][1]
+    conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
 }
 handler.command = /اجابه$/i
 
