b4bb71fe3afdbbe8204d7275ebe28dbd6bebb988
kam0998 <ztrlz06@gmail.com>
2024-09-15 09:21:15 +0200

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -11,10 +11,12 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `مثال : \n ${usedPrefix}${command} midle of night`;
 
   let search = await yts(text);
+  if (!search || search.videos.length === 0) throw 'Video Not Found, Try Another Title';
+
   let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
-  if (!search) throw 'Video Not Found, Try Another Title';
   let { title, thumbnail, timestamp, views, ago, url } = vid;
   let wm = ' 💝 ZOZO 💝'; 
+  let author = 'ZOZO';
 
   let captvid = `*❖───┊ ♪ يــوتـــيــوب ♪ ┊───❖*
     *↜❍ الـعـنوان:* ${title}
@@ -23,29 +25,26 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   *↜❍  الـرابــط:* ${url}
   *ـــــــــــــــــــــــ☽ ZOZO ☾ـــــــــــــــــــــ*`;
 
-  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
-
+  await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
 
   const audioStream = ytdl(url, {
     filter: 'audioonly',
     quality: 'highestaudio',
   });
 
-  // Get the path to the system's temporary directory
   const tmpDir = os.tmpdir();
+  const filePath = `${tmpDir}/${title}.mp3`;
 
-  // Create writable stream in the temporary directory
-  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
+  const writableStream = fs.createWriteStream(filePath);
 
-  // Start the download
   await streamPipeline(audioStream, writableStream);
 
   let doc = {
     audio: {
-      url: `${tmpDir}/${title}.mp3`
+      url: filePath
     },
     mimetype: 'audio/mp4',
-    fileName: `${title}`,
+    fileName: `${title}.mp3`,
     contextInfo: {
       externalAdReply: {
         showAdAttribution: true,
@@ -61,22 +60,20 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
   await conn.sendMessage(m.chat, doc, { quoted: m });
 
-  // Delete the audio file
-  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+  fs.unlink(filePath, (err) => {
     if (err) {
       console.error(`Failed to delete audio file: ${err}`);
     } else {
-      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+      console.log(`Deleted audio file: ${filePath}`);
     }
   });
 };
 
 handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = ['mp3', 'songs', 'ytmp3doc','اغنية2']
+handler.command = ['mp3', 'songs', 'ytmp3doc', 'اغنية2'];
 
 handler.exp = 0;
 handler.diamond = false;
 
 export default handler;
-    
