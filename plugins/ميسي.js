92dd75ebd96cbc70291eecfdb163b9f0b43bbbcf
kam0998 <ztrlz06@gmail.com>
2024-09-16 15:15:10 +0200

    Update ميسي.js

--- plugins/ميسي.js
+++ plugins/ميسي.js
@@ -1,55 +1,10 @@
-import{ prepareWAMessageMedia } from '@whiskeysockets/baileys';
-import pkg from '@whiskeysockets/baileys';
-import axios from 'axios';
-const { generateWAMessageFromContent, proto } = pkg
-const handler = async (m, { conn, usedPrefix, command }) => {
-    // جلب بيانات كريستيانو رونالدو من الملف JSON
-    const messi = (await axios.get('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json')).data;
-    const goat = messi[Math.floor(messi.length * Math.random())];
-
-    // إرسال رد فعل الرموز التعبيرية
-    await conn.sendMessage(m.chat, { react: { text: '🐐', key: m.key } });
-
-    // إعداد رسالة الوسائط
-    const mediaMessage = await prepareWAMessageMedia({ image: { url: goat } }, { upload: conn.waUploadToServer });
-
-    let msg = generateWAMessageFromContent(m.chat, {
-  viewOnceMessage: {
-    message: {
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: "*ميسي عمك 🤙🏻♥*"
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "❬ 𝒃𝒐𝒕 𝒆𝒍 𝒕𝒂𝒓𝒃𝒐𝒐 ❭"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: "*〘 THE GOAT 〙*",
-            subtitle: "",
-            hasMediaAttachment: true, 
-            imageMessage: mediaMessage.imageMessage,  
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-                {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"〘 الــتـــــالـي 〙\",\"id\":\".ليو\"}"
-             }, 
-                {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"〘 الـــــــدعــــم 〙\",\"id\":\".الدعم\"}"
-              }
-           ],
-          }) 
-        }) 
-       } 
-     } 
-   },{}) 
-    // إرسال الرسالة
-    await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
-    } 
-handler.help = ['messi', 'goat', 'ليو'];
-handler.tags = ['internet'];
-handler.command = /^(ميسي|ليو|جوت|معزه|ماء|goat)$/i;
-
-export default handler;
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*عمك مغزه 😂💕*`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['مسي', 'ميسي']
+handler.tags = ['internet']
+handler.command = /^(ميسي|مسي)$/i
+export default handler
