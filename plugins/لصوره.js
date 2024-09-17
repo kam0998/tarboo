1f4ab5f5c004d2c902e3608e04bc313eb1e3ca0d
kam0998 <ztrlz06@gmail.com>
2024-09-17 02:05:40 +0200

    Update لصوره.js

--- plugins/لصوره.js
+++ plugins/لصوره.js
@@ -1,17 +1,18 @@
-import { webp2png } from '../lib/webp2mp4.js'
+import {webp2png} from '../lib/webp2mp4.js';
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `✳️ قم بالرد على ملصق بـ:\n\n *${usedPrefix + command}*`
-    if (!m.quoted) throw notStickerMessage
-    const q = m.quoted || m
-    let mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) throw notStickerMessage
-    let media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ ها هو*', m)
-}
-handler.help = ['toimg <sticker>']
-handler.tags = ['sticker']
-handler.command = ['لصوره', 'jpg', 'aimg'] 
 
-export default handler
+const handler = async (m, {conn, usedPrefix, command}) => {
+  
+  const notStickerMessage = `*فين الاستيكر اللي عاوز تحولو ي حوب 🧞‍♂️*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw `*دا مش استيكر ي حوب 🧞‍♂️*`;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['toimg', 'لصورة'];
+export default handler;
